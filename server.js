b65072f644fb16e984006b5f13d69680dfc61e6c
Mohamed Abd El Rahman <me2580357@gmail.com>
2025-09-26 22:19:25 +0300

    fix: updated server.js and package files

--- server.js
+++ server.js
@@ -1,40 +1,34 @@
-const express=require('express'); 
-const cors=require('cors');
-const app=express();
+const express = require("express");
+const cors = require("cors");
+const app = express();
 app.use(cors());
-app.get('/',(req,res)=>{res.send("Timestamp Microservice is running")});
-app.get('/api/:date',(req,res)=>{
-    let dateString=req.params.date;
-    if(!dateString){
-        let currentDate=new Date();
-        return res.json({
-            unix:currentDate.getTime(),
-            utc:currentDate.toUTCString()
-        });
-    }
-    let date;
-    if(/^\d+$/.test(dateString)){
-        date=new Date(parseInt(dateString));
-    } else {
-        date=new Date(dateString);
-    }
-    if(date.toString()==="Invalid Date"){
-        return res.json({error:"Invalid Date"});
-    }
-    return res.json({
-        unix:date.getTime(),
-        utc:date.toUTCString()
-    });
-});
-app.get("/api", (req, res) => {
-  let currentDate = new Date();
-  return res.json({
-    unix: currentDate.getTime(),
-    utc: currentDate.toUTCString(),
-  });
+app.get("/", (req, res) => {
+  res.send("Timestamp Microservice is running");
 });
 
-const port=3000;
-app.listen(port,()=>{
-    console.log(`Server is running on port ${port}`);
+
+app.get("/api/:date?", (req, res) => {
+  let dateString = req.params.date;
+  let date;
+
+  if (!dateString) {
+    date = new Date();
+  } else if (/^\d+$/.test(dateString)) {
+
+    date = new Date(parseInt(dateString));
+  } else {
+
+    date = new Date(dateString);
+  }
+  if (date.toString() === "Invalid Date") {
+    return res.json({ error: "Invalid Date" });
+  }
+  return res.json({
+    unix: date.getTime(),
+    utc: date.toUTCString(),
+  });
+});
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
